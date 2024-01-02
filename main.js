c01d4335f726bb7892f0c8c44f5a5846d54c2ab0
Existence <andrewcarlson789@gmail.com>
2024-01-01 18:47:51 -0600

    minor changes: added another button that is still subject to glitches.

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ var upgrade = 0;
 var cs1 = 10;
 var cs2 = 100;
 var uppower = 1;
+var uppower2 = 0;
 function update() {
     var counter = document.getElementById("counter");
     counter.innerHTML = "Total currency: " + total;
     var bttn1 = document.getElementById("clickup");
     bttn1.innerHTML = "This click upgrade costs: " + cs1;
+    var bttn2 = document.getElementById("clickup2");
+    bttn2.innerHTML = "This click upgrade costs and adds 10 instead of 1: " + cs2;
 
 }
 function click() {
@@ -24,6 +27,17 @@ function upgrade() {
         cs1 += 10;
         uppower++;
         update()
+    }
+}
+function upgrade2() {
+    if(total <=cs2) {
+        return alert("not enough currency");
+    }
+    if (total == cs2 || total >= cs2) {
+        total -= cs2;
+        cs2 += 50;
+        uppower2 += 10;
+        update();
     }
 }
 document.getElementById("plusone").onclick = click();
