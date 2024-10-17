175dee1870933dc5f8a8dbf5c12fe3ded6f91680
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-17 17:06:34 -0500

    made some updates to the achievements file

--- achievements.js
+++ achievements.js
@@ -4,4 +4,14 @@ export let achievements = [
         description: "You played the game for the first time",
         earned: false,
     },
+    {
+        name: "You can click",
+        description: "You clicked the button",
+        earned: false,
+    },
+    {
+        name: "Template Achievement",
+        description: "The achievement template",
+        earned: false,
+    },
 ];
