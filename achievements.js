9c814f567bdb126478a3da355bc20a4eaf1d367d
Emonora <114265017+Emonora@users.noreply.github.com>
2024-10-18 08:57:18 -0500

    added some achievements

--- achievements.js
+++ achievements.js
@@ -10,6 +10,26 @@ export let achievements = [
         earned: false,
     },
     {
+        name: "one hundred fold button",
+        description: "You clicked 100 times",
+        earned: false,
+    },
+    {
+        name: "ten thousand fold button",
+        description: "You clicked 10000 times",
+        earned: false,
+    },
+    {
+        name: "The button is in pain",
+        description: "Stop clicking please",
+        earned: false,
+    },
+    {
+        name: "Stop abusing the button",
+        description: "Stop please its in pain",
+        earned: false,
+    },
+    {
         name: "Template Achievement",
         description: "The achievement template",
         earned: false,
